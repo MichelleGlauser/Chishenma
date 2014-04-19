29a7cdf80f8db41e690b066b3c5946449dacf3e4
MichelleGlauser <michelleglauser@gmail.com>
2014-04-20 00:10:59 +0800

    Worked on user authentication.

--- chishenma_app/views.py
+++ chishenma_app/views.py
@@ -1,11 +1,95 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# from django.template import RequestContext, loader
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.core.context_processors import csrf
+from django.template import RequestContext, loader
+from django.views.generic import TemplateView
 
-from chishenma_app.models import Category, Dish, Menu, Review, Restaurant, Bookmark
+from chishenma_app.models import Category, Dish, Menu, Review, Restaurant, Bookmark, User
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['latest_articles'] = Article.objects.all()[:5]
+        return context
 
 def index(request):
-	return render(request, 'chishenma/index.html')
+	context = {}
+	populateContext(request, context)
+	return render(request, 'chishenma/index.html', context)
+
+def login(request):
+	context = {}
+	try:
+			username = request.GET['username']
+			password = request.GET['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				if user.is_active:
+					auth_login(request, user)
+				else:
+					context['error'] = 'Non active user'
+			else:
+				context['error'] = 'Wrong username or password'
+        except:
+			context['error'] = ''
+		
+        populateContext(request, context)
+        return render(request, 'chishenma/index.html', context)
+
+def logout(request):
+	context = {}
+	try:
+		auth_logout(request)
+	except:
+		context['error'] = 'Some error occured.'
+	
+	populateContext(request, context)
+	return render(request, 'index.html', context)
+
+def populateContext(request, context):
+	context['authenticated'] = request.user.is_authenticated()
+	if context['authenticated'] == True:
+		context['username'] = request.user.username
+
+
+def auth_view(request):
+	# wechat = request.POST.get('wechat_id', '')
+	username = request.POST.get('username', '')
+	password = request.POST.get('password', '')
+	user = authenticate(username=username, password=password)
+	# user = auth.authenticate(wechat_id=wechat_id, password=password)
+
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			return HttpResponseRedirect('chishenma/loggedin/')
+	else:
+		return HttpResponseRedirect('chishenma/invalid_login/')
+
+	# if user is not None:
+	# 	auth.login(request, user)
+	# 	return HttpResponseRedirect('chishenma/loggedin/')
+	# else:
+	# 	return HttpResponseRedirect('chishenma/invalid_login/')
+
+def loggedin(request):
+	return render(request, 'chishenma/loggedin.html', 
+							 {'full_name': request.user.username})
+
+def invalid_login(request):
+	return render(request, 'chishenma/invalid_login.html')
+
+def logout(request):
+	auth.logout(requets)
+	return render(request, 'chishenma/logout.html')
+
+
+
+
 
 def choose_food(request):
 	return HttpResponse("Choose your food here.")
--- chishenma_project/urls.py
+++ chishenma_project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,17 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^chishenma_app/', include('chishenma_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/$', TemplateView.as_view(template_name='index.html'), name='home'),
+    # url(r'^$', 'chishenma_app.views.index'),
+
+    # user auth urls
+    url(r'^auth/$', 'chishenma_app.views.auth_view'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout'),
+	url(r'^loggedin/$', 'chishenma_app.views.loggedin'),
+	url(r'^invalid/$', 'chishenma_app.views.invalid_login'),
+
+    # page urls
     url(r'^choose_food/$', 'chishenma_app.views.choose_food'),
 	url(r'^help_me_decide/$', 'chishenma_app.views.help_me_decide'),
 	url(r'^your_restaurants/$', 'chishenma_app.views.your_restaurants'),
