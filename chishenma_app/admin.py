b272c36ac8e1483053e0cc281220107b3cd46274
MichelleGlauser <michelleglauser@gmail.com>
2014-05-06 09:16:12 +0800

    Added Stack Overflow auth_user error solution to admin.py.

--- chishenma_app/admin.py
+++ chishenma_app/admin.py
@@ -5,6 +5,31 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 from chishenma_app.models import Category, Dish, Menu, Review, Restaurant, Bookmark, Foodie
 
+
+from models import Foodie #you can use get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import forms
+
+class MyUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    class Meta(UserCreationForm.Meta):
+        model = Foodie
+
+class MyUserAdmin(UserAdmin):  
+    add_form = MyUserCreationForm   
+
+admin.site.register(Foodie,MyUserAdmin)
+
 # These two classes for adding dishes while editing menus are not working yet.
 # Apparently inline doesn't work with ManyToManyFields, see here: http://stackoverflow.com/questions/5345673/django-no-foreignkey-but-its-a-manytomanyfield
 # class DishInline(admin.TabularInline):
