dc6ff68b0ca914ce67d8c9c88181adaf7b7dee8b
MichelleGlauser <michelleglauser@gmail.com>
2014-05-23 23:38:12 +0100

    Commented out django.contrib.gis in models.py.

--- chishenma_app/models.py
+++ chishenma_app/models.py
@@ -1,4 +1,4 @@
-from django.contrib.gis.db import models
+# from django.contrib.gis.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
